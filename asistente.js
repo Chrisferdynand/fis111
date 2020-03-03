2d684d78f604ce879fdfd2230dbce8684e70f9f2
Chrisferdynand <ferdynand_@msn.com>
2020-03-03 17:31:46 -0400

    Primer commit de mi sitio

---
+++ asistente.js
@@ -0,0 +1,13 @@
+$(function(){
+    Swal.fire({
+	  position: 'bottom-end',
+	  type: 'warning',
+	  title: '¡Importante!',
+	  width: 200,
+	  text: 'Está página es sólo para estudiantes de laboratorio de física I, de los docentes Rolando Ticona y René Torrez.',
+	  showConfirmButton: true,
+	  background: '#FFFFFF',
+	  animation: true,
+	});
+             
+});
